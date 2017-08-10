4d1664acc95fecc3c5619f6cbcd51d6a00f597ca
Michael Codner <mhcodner@gmail.com>
2017-08-10 15:46:55 +1000

    add parameters and remove unnecessary methods

--- Controllers/CaptchaController.cs
+++ Controllers/CaptchaController.cs
@@ -3,44 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using ContactAPI.Entities;
 
 namespace ContactAPI.Controllers
 {
     [Route("api/[controller]")]
     public class CaptchaController : Controller
     {
-        // GET: api/values
+        // GET: api/Captcha
         [HttpGet]
-        public IEnumerable<string> Get()
+        public Captcha Get()
         {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
-        [HttpPost]
-        public void Post([FromBody]string value)
-        {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            return new Captcha();
         }
     }
 }
--- Controllers/ContactController.cs
+++ Controllers/ContactController.cs
@@ -3,44 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
-
-// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+using ContactAPI.Entities;
 
 namespace ContactAPI.Controllers
 {
     [Route("api/[controller]")]
     public class ContactController : Controller
     {
-        // GET: api/values
-        [HttpGet]
-        public IEnumerable<string> Get()
-        {
-            return new string[] { "value1", "value2" };
-        }
-
-        // GET api/values/5
-        [HttpGet("{id}")]
-        public string Get(int id)
-        {
-            return "value";
-        }
-
-        // POST api/values
+        // POST api/Contact
         [HttpPost]
-        public void Post([FromBody]string value)
+        public void Post(ContactMessage Message)
         {
-        }
-
-        // PUT api/values/5
-        [HttpPut("{id}")]
-        public void Put(int id, [FromBody]string value)
-        {
-        }
-
-        // DELETE api/values/5
-        [HttpDelete("{id}")]
-        public void Delete(int id)
-        {
+            
         }
     }
 }
